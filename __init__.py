6ae847ec8a4eb7148c3ef42f7643a6dce29803c8
peristrophe <peristrophe@live.jp>
2019-03-19 15:51:33 +0900

    third commit

--- __init__.py
+++ __init__.py
@@ -3,10 +3,3 @@ from . import client
 
 Client = client.Communicator
 
-class AttrDict(dict):
-    def __getattr__(self, key):
-        return self.get(key, None)
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
